cdb57d592f0092025846e5a8584743be9c6dbda2
Ilyas <mukatdisovilyas@gmail.com>
2022-04-07 11:01:54 +0500

    simple function

--- app/src/main/java/com/gmail/mukatdisovilyas/clock/MainActivity.kt
+++ app/src/main/java/com/gmail/mukatdisovilyas/clock/MainActivity.kt
@@ -3,14 +3,20 @@ package com.gmail.mukatdisovilyas.clock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.gmail.mukatdisovilyas.clock.ui.theme.ClockTheme
+import kotlinx.coroutines.delay
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity()
 {
@@ -24,13 +30,38 @@ class MainActivity : ComponentActivity()
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Clock()
                 }
             }
         }
     }
 }
 
+@Composable
+fun Clock()
+{
+    val sdf = SimpleDateFormat("hh:mm:ss", Locale.getDefault())
+    var currentDate by remember {
+        mutableStateOf(sdf.format(Date()))
+    }
+    LaunchedEffect(key1 = true) { // this: CoroutineScope
+        while (true)
+        {
+            delay(1000L)
+            currentDate = sdf.format(Date())
+        }
+    }
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = currentDate, fontSize = 40.sp)
+    }
+}
+
+/*
 @Composable
 fun Greeting(name: String)
 {
@@ -44,4 +75,4 @@ fun DefaultPreview()
     ClockTheme {
         Greeting("Android")
     }
-}
+}*/
